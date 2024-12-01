b5a8ae8d89e334997cc4a77aa291dd2bdbfc86e7
Manikandan <81812564+mintnchocochip@users.noreply.github.com>
2024-12-01 17:11:34 +0530

    Day 1 challenge- Dec 1st

---
+++ summer.py
@@ -0,0 +1,27 @@
+import math
+list1 = []
+list2 = []
+sum = 0
+with open(r"C:\Users\Shawf\OneDrive\Desktop\text.txt", "r") as reader:
+    temp = reader.readlines()
+    for i in range(len(temp)):
+        str = temp[i].split(" ")
+        list1.append(int(str[0]))
+        list2.append(int(str[3]))
+list1 = sorted(list1)
+list2 = sorted(list2)
+freq = {}
+for i in range(len(list1)):
+    if list1[i] in freq:
+        print("Already in dictionary", list1[i])
+        continue
+    else:
+        freq[list1[i]] = 0
+    for j in range(len(list2)):
+        if list2[j] == list1[i]:
+            freq[list1[i]] += 1
+            print("Incrementing", list1[i])
+for val in list1:
+    print(val, freq[val])
+    sum += freq[val]*val
+print(sum)
